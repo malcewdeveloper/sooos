3ca15ea5d622c2873f44bdc5f48c385387e6af18
Maksim <malcewdeveloper@yandex.ru>
2023-09-14 23:29:48 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, 
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                mode: 'local',
+                                auto: true,
+                                localIdentName: '[folder]__[local]_[hash]',
+                                localIdentHashDigestLength: 4,
+                            }
+                        }
+                    }, 
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.svg$/,
+                use: {
+                    loader: 'svg-url-loader'
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: '/node_modules/',
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.jsx$/,
+                exclude: '/node_modules/',
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env', 
+                            '@babel/preset-react'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.tsx$/,
+                exclude: '/node_modules/',
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env', 
+                            '@babel/preset-react',
+                            '@babel/preset-typescript'
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/assets/index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        })
+    ],
+    devServer: {
+        port: 4000,
+        hot: isDev,
+        historyApiFallback: true
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts' ,'.tsx']
+    }
+}
