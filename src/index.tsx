645df0a0c0365e3c48c1eede8d3f685b16566290
Maksim <malcewdeveloper@yandex.ru>
2023-09-15 06:48:08 +0300

    Add fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
+import './assets/fonts/ProximaNova/PNSemibold.ttf';
+import './assets/fonts/Lack/Lackregular.ttf';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <h1 style={{ textAlign: "center" }}>
-        Cкоро здесь будет крутой проект
-    </h1>
-)
+root.render(<App />)
