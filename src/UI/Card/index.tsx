2664a4d827d33094501854c332a970004926dbe0
Maksim <malcewdeveloper@yandex.ru>
2023-09-18 23:26:55 +0300

    Create Card index file

---
+++ src/UI/Card/index.tsx
@@ -0,0 +1,14 @@
+export { default } from './Card';
+
+export { default as cardClasses } from './cardClasses';
+
+export { default as CardHeader } from './components/CardHeader';
+
+export { default as CardMedia } from './components/CardMedia';
+
+export { default as CardContent } from './components/CardContent';
+
+export { default as CardActions } from './components/CardActions';
+
+export { default as CardActionArea } from './components/CardActionArea';
+
