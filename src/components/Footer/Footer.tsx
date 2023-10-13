24dc27af316b7a4aa8f4c5695ecd48af90d80e8d
Maksim <malcewdeveloper@yandex.ru>
2023-10-13 18:10:17 +0300

    Change the footer layout

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,6 +3,7 @@ import clsx from "clsx";
 import List, {  ListItem } from "../../UI/List";
 import Typography from "../../UI/Typography";
 import Button from "../../UI/Button";
+import Box from "../../UI/Box";
 import LogoNoBg from '../../assets/images/logoNoBg.svg';
 import EpatajSvg from '../../assets/images/epataj.svg';
 import DesignSvg from '../../assets/images/design.svg';
@@ -11,6 +12,7 @@ import { styled } from "@mui/material";
 import { unstable_composeClasses as composeClasses } from "@mui/material";
 import { getFooterClass } from "./footerClasses";
 import { FaPaperPlane, FaEnvelope, FaCompass, FaGithub } from "react-icons/fa";
+import { useMediaQuery } from '@mui/material';
 
 
 const useUtilityClasses = (ownerState) => {
@@ -31,7 +33,13 @@ interface IFooterProps {
 const FooterRoot = styled('footer')(({ theme }) => ({
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    paddingLeft: '16px',
+    paddingRight: '16px',
+    ...(useMediaQuery(theme.breakpoints.down(444)) && {
+        paddingLeft: '8px',
+        paddingRight: '8px',
+    })
 }));
 
 const Footer: React.FC<IFooterProps> = (props) => {
@@ -50,74 +58,76 @@ const Footer: React.FC<IFooterProps> = (props) => {
         as={ component } 
         className={ clsx(classes.root, className) }
         { ...other }>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-                <List header={
-                    <div style={{ marginBottom: '24px' }}>
-                        <Typography variant='body2'>Vladislav Movchan</Typography>
-                        <Typography variant='body2' style={{ opacity: 0.5, fontSize: '14px' }}>Design</Typography>
-                    </div>
-                }>
-                    <ListItem>
-                        <Button href="mailto:@desiiign@ya.ru" startIcon={ <FaEnvelope size={14} /> } style={{ color: '#FFFFFF' }}>
-                            <Typography variant='body2'>@desiiign@ya.ru</Typography>
-                        </Button>
-                    </ListItem>
-                    <ListItem>
-                        <Button href="https://t.me/@moovchaan" startIcon={ <FaPaperPlane size={14} /> } style={{ color: '#FFFFFF' }}>
-                            <Typography variant='body2'>@moovchaan</Typography>
-                        </Button>
-                    </ListItem>
-                    <ListItem>
-                        <Button href="https://www.desiiign.ru" startIcon={ <FaCompass size={14} /> } style={{ color: '#FFFFFF' }}>
-                            <Typography variant='body2' style={{ color: '#97F223' }}>desiiign.ru</Typography>
-                        </Button>
-                    </ListItem>
-                </List>
-                <div>
-                    <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', margin: '0 57px 0 47px' }}>
-                        <div style={{ 
-                            backgroundImage: `url("${ FooterLogoBg }")`, 
-                            backgroundRepeat: 'no-repeat',
-                            backgroundPosition: 'center center',
-                            display: 'flex',
-                            justifyContent: 'center',
-                            width: '132px', 
-                            height: '132px'
-                        }}>
-                            <LogoNoBg style={{ position: 'absolute', marginTop: '71px' }} />
-                        </div>
-                        <Typography variant='body2' style={{ opacity: 0.5, margin: '17px 0 52px' }}>IT Помощник</Typography>
-                        <div style={{ display: 'flex', alignItems: 'center', gap: '35px', marginBottom: '15px' }}>
-                            <EpatajSvg />
-                            <DesignSvg />
-                        </div>
-                        <Typography variant='body2' style={{ marginBottom: '15px', opacity: 0.5 }}>www.sooos.ru</Typography>
-                        <Typography variant='body2' style={{ opacity: 0.5 }}>2023</Typography>
-                    </div>
-                </div>
-                <List header={
-                    <div style={{ marginBottom: '24px' }}>
-                        <Typography variant='body2'>Maxim Lukhovskiy</Typography>
-                        <Typography variant='body2' style={{ opacity: 0.5, fontSize: '14px' }}>Developer</Typography>
-                    </div>
-                }>
-                    <ListItem>
-                        <Button href="mailto:malcewdeveloper@ya.ru" startIcon={ <FaEnvelope size={14} /> } style={{ color: '#FFFFFF' }}>
-                            <Typography variant='body2'>malcewdeveloper@ya.ru</Typography>
-                        </Button>
-                    </ListItem>
-                    <ListItem>
-                        <Button href="https://t.me/@malcew" startIcon={ <FaPaperPlane size={14} /> } style={{ color: '#FFFFFF' }}>
-                            <Typography variant='body2'>@malcew</Typography>
-                        </Button>
-                    </ListItem>
-                    <ListItem>
-                        <Button href="https://github.com/malcewdeveloper" startIcon={ <FaGithub size={14} /> } style={{ color: '#FFFFFF' }}>
-                            <Typography variant='body2' style={{ color: '#97F223' }}>GitHub</Typography>
-                        </Button>
-                    </ListItem>
-                </List>
-            </div>
+            <Box style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', gap: '24px' }}>
+                <Box style={{ textAlign: 'center' }}>
+                    <Box style={{ 
+                        backgroundImage: `url("${ FooterLogoBg }")`, 
+                        backgroundRepeat: 'no-repeat',
+                        backgroundPosition: 'center center',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        width: '132px', 
+                        height: '132px'
+                    }}>
+                        <LogoNoBg style={{ position: 'absolute', marginTop: '71px' }} />
+                    </Box>
+                    <Typography variant='body2' style={{ opacity: 0.5 }}>IT Помощник</Typography>
+                </Box>
+                <Box style={{ display: 'flex', textAlign: 'center' }}>
+                    <List header={
+                        <Box style={{ marginBottom: '16px' }}>
+                            <Typography variant='body2'>Vladislav Movchan</Typography>
+                            <Typography variant='body2' style={{ opacity: 0.5, fontSize: '14px' }}>Design</Typography>
+                        </Box>
+                    }>
+                        <ListItem>
+                            <Button href="mailto:@desiiign@ya.ru" startIcon={ <FaEnvelope size={14} /> } style={{ color: '#FFFFFF' }}>
+                                <Typography variant='body2'>@desiiign@ya.ru</Typography>
+                            </Button>
+                        </ListItem>
+                        <ListItem>
+                            <Button href="https://t.me/@moovchaan" startIcon={ <FaPaperPlane size={14} /> } style={{ color: '#FFFFFF' }}>
+                                <Typography variant='body2'>@moovchaan</Typography>
+                            </Button>
+                        </ListItem>
+                        <ListItem>
+                            <Button href="https://www.desiiign.ru" startIcon={ <FaCompass size={14} /> } style={{ color: '#FFFFFF' }}>
+                                <Typography variant='body2' style={{ color: '#97F223' }}>desiiign.ru</Typography>
+                            </Button>
+                        </ListItem>
+                    </List>
+                    <List header={
+                        <Box style={{ marginBottom: '16px' }}>
+                            <Typography variant='body2'>Maxim Lukhovskiy</Typography>
+                            <Typography variant='body2' style={{ opacity: 0.5, fontSize: '14px' }}>Developer</Typography>
+                        </Box>
+                    }>
+                        <ListItem>
+                            <Button href="mailto:malcewdeveloper@ya.ru" startIcon={ <FaEnvelope size={14} /> } style={{ color: '#FFFFFF' }}>
+                                <Typography variant='body2'>malcewdeveloper@ya.ru</Typography>
+                            </Button>
+                        </ListItem>
+                        <ListItem>
+                            <Button href="https://t.me/@malcew" startIcon={ <FaPaperPlane size={14} /> } style={{ color: '#FFFFFF' }}>
+                                <Typography variant='body2'>@malcew</Typography>
+                            </Button>
+                        </ListItem>
+                        <ListItem>
+                            <Button href="https://github.com/malcewdeveloper" startIcon={ <FaGithub size={14} /> } style={{ color: '#FFFFFF' }}>
+                                <Typography variant='body2' style={{ color: '#97F223' }}>GitHub</Typography>
+                            </Button>
+                        </ListItem>
+                    </List>
+                </Box>
+                <Box style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', margin: '0 57px 0 47px' }}>
+                    <Box style={{ display: 'flex', alignItems: 'center', gap: '35px', marginBottom: '15px' }}>
+                        <EpatajSvg />
+                        <DesignSvg />
+                    </Box>
+                    <Typography variant='body2' style={{ marginBottom: '15px', opacity: 0.5 }}>www.sooos.ru</Typography>
+                    <Typography variant='body2' style={{ opacity: 0.5 }}>2023</Typography>
+                </Box>
+            </Box>
         </FooterRoot>
     )
 }
